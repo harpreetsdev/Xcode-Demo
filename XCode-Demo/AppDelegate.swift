4a9b7c047cce2f440a722a90c84605a158c66c94
Harpreet Singh <harpreetsdevelopers@gmail.com>
2017-09-03 12:32:53 -0400

    Print statements for App Delegate global functions.

--- XCode-Demo/AppDelegate.swift
+++ XCode-Demo/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        print("App Delegate function \"didFinishLaunchingWithOptions\" got invoked")
         return true
     }
 
@@ -28,20 +29,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        print("App Delegate function \"applicationDidEnterBackground\" got invoked")
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        print("App Delegate function \"applicationWillEnterForeground\" got invoked")
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        print("App Delegate function \"applicationDidBecomeActive\" got invoked")
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
         self.saveContext()
+        print("App Delegate function \"applicationWillTerminate\" got invoked")
     }
 
     // MARK: - Core Data stack
